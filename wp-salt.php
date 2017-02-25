23cee0988c8d9dc6f9e21385e5e3817138fa2b4b
Ramon <Ramon.Cisternas@axa-tech.com>
2017-02-25 11:11:24 +0100

    wp-salt.php

---
+++ wp-salt.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         '#j)84%I6@Xu-axica&-y,pNZ34z&IbN7oW*8 YNl3>3tIgdF3VY7j^:`J2|{)J#v');
+define('SECURE_AUTH_KEY',  '<dDp-3GaZK=4|e~~I:)A.e3_V3b#3m8R?B#OR<:cWFb2IBuCbJCu9yB-+rHgOy8}');
+define('LOGGED_IN_KEY',    '?YZ?K}p_:i9$q,>bc-.L-F|A&WBqU=|V6/S?+9fQS/G3e5N{SJ!y_x=7#-N#-Mv#');
+define('NONCE_KEY',        'k/(ZW{x^qY*)?iCGy^L*m&>8K(J8Dp+7_$vMXa,d3abC#hkU|bp2:W!tOUyMiOJa');
+define('AUTH_SALT',        '|Sx5WDB)6&Ic5K-iVQ4U]6d6:6ZpCmU}(_I6;5K~va%E[5goV{ kN),a2Y ~n6$L');
+define('SECURE_AUTH_SALT', 'JTyW}=<B@W5JwlRF*`A7e2[;q<e-;L40i&e64 }aZkg(b$Q|yK$0UuU+8)^)Jcz0');
+define('LOGGED_IN_SALT',   'luer{y9fkmA^C05Y:tX>.GaW0h+jJ}cRX5H~d-kplTVPr-G*-VYbhqb31R%%DeFQ');
+define('NONCE_SALT',       'rbjt-{I<2vUwi`7~oD>vbJmbuk0F%V>~+#G{h)q[DMm:V!Ve|*CnT>}h|hKr!DH=');
